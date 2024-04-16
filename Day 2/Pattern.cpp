1a2e40106cb525a9010f30f12bdda6f6ac3b5219
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-16 23:06:47 +0530

    changes

--- Day 2/Pattern.cpp
+++ Day 2/Pattern.cpp
@@ -82,7 +82,25 @@ int main(){
 
 
 // Pyramid 
-  int n;
+//   int n;
+//   cin>> n;
+
+//   for(int row=0; row<n; row=row+1){
+//   for(int col=0; col<n-row-1; col=col+1){
+//     cout<< " ";
+//   }
+
+//   for(int col=0; col<row+1; col=col=col+1){
+//     cout<< "* ";
+//   }
+
+//   cout<< endl;
+//   }
+
+//   return 0;
+// }
+
+ int n;
   cin>> n;
 
   for(int row=0; row<n; row=row+1){
